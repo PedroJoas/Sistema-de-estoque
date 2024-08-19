02ecf6a3c7dc6f90279449e3dc575f3044da94e4
PedroJoas <pjoas26@gmail.com>
2024-08-19 20:47:23 -0300

    feat: implementando adicionar da classe estoque

--- src/classes/Estoque.java
+++ src/classes/Estoque.java
@@ -1,6 +1,10 @@
 package classes;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import classes.Produto;
@@ -15,6 +19,7 @@ public class Estoque {
      */
 
     private String caminho_estoque = "data/estoque.txt";
+    BufferedWriter writer;
 
     public Estoque(){
         try {
@@ -24,10 +29,64 @@ public class Estoque {
             } 
 
         } catch (IOException e) {
-            System.out.println("Error: Failed create file " + e.getMessage());
+            System.out.println("Error: Falha ao criar arquivo " + e.getMessage());
         }
     }
 
-    public void listarItens(){}
+    public void listarItens(){
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(caminho_estoque));
+
+            reader.close();
+        } catch (Exception e) {
+
+        }
+    }
+
+    public void adicionarProduto(Produto produto){
+        Boolean verificaSeExiste = buscarItem(produto);
+
+        if(!verificaSeExiste){
+            String nome = produto.getNome();
+            String categoria = produto.getCategoria();
+            float preco = produto.getPreco();
+            int quantidade = 1;
+
+            // Juntar no formato separado por vírgula
+            String linha = nome + "," + categoria + "," + preco + "," + quantidade;
+            try {
+                // Escrever no arquivo
+                writer = new BufferedWriter(new FileWriter(caminho_estoque, true));
+                writer.write(linha);
+                writer.newLine();
+            
+                // Fechar o Writer após a escrita
+                writer.close();
+                
+            } catch (IOException e) {
+                System.out.println("Error: Erro ao escrever no arquivo "+ e.getMessage());
+            }
+
+        }else{
+            aumentarQuantidade(produto);
+        }
+        
+
+    }
+    private void aumentarQuantidade(Produto produto){
+
+    }
+    public void removerItem(Produto produto){
+        Boolean verifica = buscarItem(produto);
+    }
+    
+    public void editarItem(Produto produto){
+
+    }
+    private Boolean buscarItem(Produto produto){
+        return true;
+    }
+
 
 }
